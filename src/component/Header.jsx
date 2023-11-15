051ccc5945fc3743345d737e2baf391d44aae0ce
rkamalraj <rkamalraj12345@gmail.com>
2023-11-15 17:16:10 +0530

    outer process

---
+++ src/component/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useState} from 'react'
+
+const container = () => {
+  const [name,setName ]=useState
+  const namechenging = () => {
+  const chengingName =["Partha", "Sarathy", "Dinesh", "Kamal"]
+  const nameMathad=[Math.floor(Math.random()*chengingName.length)]
+  setName(chengingName[nameMathad])
+}
+  
+  retur (
+    <div>
+    <p>Hii{name}</p>
+    <button onClick={namechenging}>check mee</button>
+    </div>
+  )
+}
+
+
+export default Header
